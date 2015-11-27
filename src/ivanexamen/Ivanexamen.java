a14744c7ea112105a9013fbb1f3b1cd72773dd6f
adaw <adaw@smxubuntu>
2015-11-27 17:59:08 +0100

    Versió FINALITZADAAAA del projecte ivanexamen

--- src/ivanexamen/Ivanexamen.java
+++ src/ivanexamen/Ivanexamen.java
@@ -19,6 +19,8 @@ public class Ivanexamen {
      */
     public static void main(String[] args) {
         System.out.print("ivanbonnin");
+               System.out.print("FINISHHHHHHHHH");
+               System.out.print("FUA MA PAAAAAAA");
     }
     
 }
